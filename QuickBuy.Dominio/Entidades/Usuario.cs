d39ab0d6d99c62e7bcb1f018238f4d8c95229ebe
Gabriel <gabrielmajaron@gmail.com>
2020-11-04 14:14:09 -0300

    Criando Entidades

---
+++ QuickBuy.Dominio/Entidades/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    class Usuario
+    {
+        public int ID { get; set; }
+
+        public string Email { get; set; }
+
+        public string Senha { get; set; }
+
+        public string Nome { get; set; }
+
+        public string SobreNome { get; set; }
+
+        //public List<Pedido> Pedidos {get; set;}
+        // Um Usuario pode ter nenhum ou muitos pedidos
+        public ICollection<Pedido> Pedidos { get; set; }
+
+        // Um Pedido pode ter um item ou muitos itens
+        public ICollection<ItemPedido> ItensPedido { get; set; }
+    }
+}
---
+++ QuickBuy.Dominio/ObjetoDeValor/FormaPagamento.cs
@@ -0,0 +1,34 @@
+﻿using QuickBuy.Dominio.Enumerados;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades.ObjetoDeValor
+{
+    public class FormaPagamento
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public bool isTipoBoleto
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.Boleto; }
+        }
+
+        public bool isTipoCartaoCredito
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.CartaoCredito; }
+        }
+
+        public bool isTipoNaoDefinido
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.NaoDefinido; }
+        }
+
+        public bool isTipoTransferencia
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.Transferencia; }
+        }
+
+    }
+}
